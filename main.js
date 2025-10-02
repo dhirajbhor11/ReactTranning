1a04448b2a1f8c0c89589ad31b22e5c6ed6ba37a
dhirajbhor11 <dhirajbhor11@gmail.com>
2025-10-02 22:07:21 +0530

    Revert "added divide method, added console log in that method"
    This reverts commit 381f27a437dc116dd99ec0f7fb28820cd7935b6e.

--- main.js
+++ main.js
@@ -13,13 +13,4 @@ function multiplyTwoNumber(a, b) {
     return a * b;
 }
 
-function divideTwoNumber(a, b) {
-    if (b === 0) {
-        console.error("Division by zero is not allowed.");
-        return null;
-    }
-    console.log("Dividing", a, "by", b);
-    return a / b;
-}
-
 console.log(add(2, 3));
